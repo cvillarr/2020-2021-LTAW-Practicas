4c0194cf84e07d8d0a6a72276670994a84bd1abc
cvillarr <c.villarr@alumnos.urjc.es>
2021-05-23 20:55:57 +0200

    problemitas con los mensajes

--- P3/public/cliente.js
+++ P3/public/cliente.js
@@ -1,19 +1,21 @@
-//-- Elementos del interfaz
+//Elementos del interfaz
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
 
-//-- Crear un websocket. Se establece la conexión con el servidor
+//Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
 
 socket.on("message", (msg)=>{
   display.innerHTML += '<p style="color:black">' + msg + '</p>';
+  console.log("llego");
 });
 
-//-- Al apretar el botón se envía un mensaje al servidor
+//Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
-  if (msg_entry.value)
+  if (msg_entry.value) {
     socket.send(msg_entry.value);
-  
-  //-- Borrar el mensaje actual
+    console.log("y aqui tmb");
+  }
+  //Borrar el mensaje actual
   msg_entry.value = "";
 }
