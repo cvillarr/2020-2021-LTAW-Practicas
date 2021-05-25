4691371bae7525a141722f87af8d86e506dfc916
cvillarr <c.villarr@alumnos.urjc.es>
2021-05-25 23:33:40 +0200

    nick implementado

--- P3/public/cliente.js
+++ P3/public/cliente.js
@@ -1,9 +1,13 @@
 //Elementos del interfaz
 const display = document.getElementById("display");
 const msg_entry = document.getElementById("msg_entry");
+const msg_nick = document.getElementById("msg_nick");
 
 //Cargamos el tono
 let tono = new Audio('tono.mp3');
+
+//Creamos variable para el nick 
+let nick = 'Anónimo';
 
 //Crear un websocket. Se establece la conexión con el servidor
 const socket = io();
@@ -17,8 +21,15 @@ socket.on("message", (msg)=>{
 //Al apretar el botón se envía un mensaje al servidor
 msg_entry.onchange = () => {
   if (msg_entry.value) {
-    socket.send(msg_entry.value);
+    socket.send(nick + ":" + msg_entry.value);
   }
   //Borrar el mensaje actual
   msg_entry.value = "";
+}
+
+msg_nick.onchange = () => {
+  if (msg_nick.value){
+    nick = msg_nick.value;
+  }
+  console.log(nick);
 }
